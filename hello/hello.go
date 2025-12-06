863fba41db9c1a41dfa27e0a14c2f8530afd1eab
jw <choi.jae100@gmail.com>
2025-12-05 17:54:52 -0800

    add hello

---
+++ hello/hello.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+	prefix := greetingPrefix(lang)
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
+}
+
+func main() {
+	fmt.Println(Hello("Jae", ""))
+}
